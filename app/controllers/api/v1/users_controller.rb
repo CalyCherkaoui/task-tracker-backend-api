73b8a1748080f0d6d8c0ecf86e45c8588f00457e
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-16 15:30:02 -0500

    Refactor show action in users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,15 +2,22 @@ module Api
   module V1
     class UsersController < ApplicationController
       before_action :authenticate_user!
+      before_action :set_user
 
       def show
-        render json: { message: 'Yeppa you did it', username: @user.username, email: @user.email }
+        if current_user.id == @user.id
+          render json: { message: 'Welcome to your profile',
+                         username: @user.username,
+                         currentuser: current_user.username }
+        else
+          render json: { message: 'You are not authorize!', currentuser: current_user.username }
+        end
       end
 
       private
 
       def set_user
-        @user = cureent_user!
+        @user = User.find(params[:id])
       end
     end
   end
