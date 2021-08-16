00f4677f66d1c67bb8a91552c26011a10cddce1e
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-16 12:46:24 -0500

    Implement the routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1, defaults: { format: :json} do
+      resources :users, only: %w[show]
+    end
+  end
+
+  devise_for :users,
+    defaults: { format: :json },
+    path: '',
+    path_names: {
+      sign_in: 'api/v1/login',
+      sign_out: 'api/v1/logout',
+      registration: 'api/v1/signup'
+    },
+    controllers: {
+      sessions: 'sessions',
+      registrations: 'registrations'
+    }
 end
