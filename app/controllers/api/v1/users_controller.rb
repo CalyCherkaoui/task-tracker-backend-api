ce6bd904f4020d31e4f47c5aa89bc75d8df08341
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-16 13:07:37 -0500

    Implement users controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,17 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+      before_action :authenticate_user!
+    
+      def show
+        render json: { message: "Yeppa you did it", email: @user.email }
+      end
+
+      private
+
+      def set_user
+        @user = cureent_user!
+      end
+    end
+  end
+end
