9fab155adf54d8712fffeee71e657fcd7b58ec8c
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-16 13:01:19 -0500

    Implement registerations controller

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,19 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+
+  private
+
+  def respond_with(resource, _opts = {})
+    register_success && return if resource.persisted?
+
+    register_failed
+  end
+
+  def register_success
+    render json: { message: 'Amazing! You have signed up sucessfully.', logged?: true }
+  end
+
+  def register_failed
+    render json: { message: "Opps! Something went wrong. Try to signup again.", logged?: false }
+  end
+end
