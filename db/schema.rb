b2edde75d05fc984bf5f7aad01dbee3f72e919bd
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-16 11:32:59 -0500

    Migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_16_134530) do
+ActiveRecord::Schema.define(version: 2021_08_16_141719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jwt_denylist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "expired_at", null: false
+    t.index ["jti"], name: "index_jwt_denylist_on_jti"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
