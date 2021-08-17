67fe16242062a0f9f9ed5e3741cd1a7aa7b661bc
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-17 16:06:55 -0500

    Refactor sessions controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -13,36 +13,38 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   def respond_to_on_destroy
-    log_out_success && return if current_user
+    # log_out_success && return if current_user
 
-    log_out_failure
+    # log_out_failure
+    head :ok
   end
 
-  def log_out_success
-    render jsonapi: success_logout_message, status: :ok
-  end
+  # def log_out_success
+  #   render jsonapi: success_logout_message, status: :ok
+  # end
 
-  def log_out_failure
-    render jsonapi: errors_response, status: :unauthorized
-  end
+  # def log_out_failure
+  #   render jsonapi: errors_response, status: :unauthorized
+  # end
 
-  def errors_response
-    render json: {
-      errors:
-        [
-          { message: 'Oops! nothing happened. Try to logout again!.' }
-        ],
-      title: 'Log-out failure'
-    }
-  end
+  # def login_success_message
+  #   {
+  #     message: 'Greate! Now you are logged in',
+  #     msg_title: 'login_success'
+  #   }
+  # end
 
-  def success_logout_message
-    render json: {
-      message:
-        [
-          { message: 'You are logged out. See you soon!' }
-        ],
-      title: 'Successfuly logged out'
-    }
-  end
+  # def logout_failure_message
+  #   {
+  #     message: 'Oops! nothing happened. Try to logout again!.',
+  #     msg_title: 'logout_failure'
+  #   }
+  # end
+
+  # def logout_success_message
+  #   {
+  #     message: 'You are logged out. See you soon!',
+  #     msg_title: 'logout_success'
+  #   }
+  # end
 end
