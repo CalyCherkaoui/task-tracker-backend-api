9af7436471b6812268ac2f54f1610b08a09e13de
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-17 16:07:17 -0500

    Refactor CORS config

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,14 @@ module TaskTrackerBackendApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+
+        resource '*',
+          headers: :any,
+          methods: %i[get patch put delete post options head]
+      end
+    end
   end
 end
