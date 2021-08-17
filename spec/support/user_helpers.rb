7c368e6860a051b03fffe9f330cf1bf173573f59
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-17 13:46:49 -0500

    Implement user spec helpers methods

---
+++ spec/support/user_helpers.rb
@@ -0,0 +1,24 @@
+require 'faker'
+require 'factory_bot_rails'
+
+module UserHelpers
+
+  def create_user
+    FactoryBot.create(
+            :user,
+            username: Faker::Internet.username,
+            email: Faker::Internet.email, 
+            password: Faker::Internet.password
+        )
+  end
+
+    def build_user
+    FactoryBot.build(
+            :user,
+            username: Faker::Internet.username,
+            email: Faker::Internet.email, 
+            password: Faker::Internet.password
+        )
+  end
+
+end
