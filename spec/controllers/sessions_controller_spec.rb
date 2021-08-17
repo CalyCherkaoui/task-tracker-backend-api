f177d6db93896c3b30174e73f30c233e6a867757
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-17 17:44:18 -0500

    Implement sessions controller test series

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe 'SessionsController', type: :request do
+
+  let (:user) { create_user }
+  let (:login_url) { '/api/v1/login' }
+  let (:logout_url) { '/api/v1/logout' }
+
+  context 'When logging in' do
+    before do
+      login_with_api(user)
+    end
+
+    it 'returns an authorization token' do
+      expect(response.headers['Authorization']).to be_present
+    end
+
+    it 'returns ok http status 200' do
+      expect(response.status).to eq(200)
+    end
+  end
+
+  context 'When password is missing' do
+    before do
+      post login_url, params: {
+        user: {
+          username: user.username,
+          email: user.email,
+          password: nil
+        }
+      }
+    end
+
+    it 'returns http status 401' do
+      expect(response.status).to eq(401)
+    end
+
+  end
+
+  context 'When logging out' do
+    before do
+      login_with_api(user)
+    end
+
+    it 'returns http status 204' do
+      delete "/api/v1/logout", headers: { 'Authorization' => response.headers['Authorization']}
+
+      expect(response).to have_http_status(200)
+    end
+  end
+
+end
