0acbbc020b57f2d365dccc938b710ecf495d28aa
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-17 13:55:32 -0500

    Configure Jsonapi-rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,8 +5,9 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'jsonapi/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
-
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -62,4 +63,5 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include UserHelpers
+  config.include JSONAPI::RSpec
 end
