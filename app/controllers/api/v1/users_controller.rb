a493900b23846d176abebb1cd70def9bf14cbc67
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-17 16:40:34 -0500

    Refactor error rendering

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,24 +1,25 @@
-# module Api
-#   module V1
-class API::V1::UsersController < Api::V1::BaseController
-  before_action :set_user, only: %w[show]
+module Api
+  module V1
+    class UsersController < ApplicationController
+      before_action :authenticate_user!
+      before_action :set_user, only: %w[show]
 
-  def show
-    if current_user.id == @user.id
-      # render json: { message: 'Welcome to your profile',
-      #                 username: @user.username,
-      #                 currentuser: current_user.username }
-      render jsonapi: @user, status: :ok
-    else
-      render jsonapi: errors_not_authorized
+      def show
+        if current_user.id == @user.id
+          # render json: { message: 'Welcome to your profile',
+          #                 username: @user.username,
+          #                 currentuser: current_user.username }
+          render_jsonapi_response(current_user)
+        else
+          errors_not_authorized
+        end
+      end
+
+      private
+
+      def set_user
+        @user = User.find(params[:id])
+      end
     end
   end
-
-  private
-
-  def set_user
-    @user = User.find(params[:id])
-  end
 end
-#   end
-# end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::API
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+
   def render_jsonapi_response(resource)
     if resource.errors.empty?
       render jsonapi: resource, status: 200
@@ -8,6 +11,44 @@ class ApplicationController < ActionController::API
       render jsonapi_errors: resource.errors, status: 400
     end
   end
+
+  def not_found
+    render json: {
+      errors: [
+        {
+          message: 'Oops! Not found',
+          status: '404',
+          title: 'Not_found'
+        }
+      ]
+    }, status: 404
+  end
+
+  def errors_not_authorized
+    render json: {
+      errors:
+        [
+          {
+            message: 'Oops! Admin only! Sorry! You are not authorized!',
+            title: 'Not_authorised_user',
+            status: 401
+          }
+        ],
+    }, status: 401
+  end
+
+  def record_invalid
+    render json: {
+      'errors': [
+        {
+          message: 'Oops! something went wrong!',
+          status: '400',
+          title: 'Invalid Record'
+        }
+      ]
+    }, status: 400
+  end
+
 
   protected
 
