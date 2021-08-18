2cf048ade22272a626e8a52b7d1e6974a146dbe3
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-18 16:33:46 -0500

    Update routine serializable

--- app/serializable/serializable_routine.rb
+++ app/serializable/serializable_routine.rb
@@ -3,6 +3,18 @@ class SerializableRoutine < JSONAPI::Serializable::Resource
 
   attributes :id, :name, :icon, :priority
 
+  attribute :ordered_tasks do
+    @object.ordered_tasks
+  end
+
+  attribute :hero_task do
+    @object.hero_task
+  end
+
+  attribute :tasks_count do
+    @object.tasks_count
+  end
+
   link :self do
     @url_helpers.api_v1_routine_path(@object.id)
   end
