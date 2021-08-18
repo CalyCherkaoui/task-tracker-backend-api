b7eda8270f44f8c28868b482bc175cda5a891365
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-18 16:33:31 -0500

    Implement routine model with scopes

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -1,4 +1,16 @@
 class Routine < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3 }
-  validates :priority, presence: true
+  validates :priority, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  def ordered_tasks
+    tasks.ordered
+  end
+
+  def hero_task
+    tasks.ordered.first
+  end
+
+  def tasks_count
+    tasks.count
+  end
 end
