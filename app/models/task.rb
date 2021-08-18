2ce4dd01e091eb2d57ccd59d518b15c86a332e6b
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-18 16:40:19 -0500

    Update tsk model with scopes

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,13 @@ class Task < ApplicationRecord
   belongs_to :routine
   belongs_to :user
   validates :name, presence: true, length: { minimum: 2 }
-  validates :unit, :priority, :goal, presence: true
+  validates :unit, :goal, presence: true
+  validates :priority, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  scope :priority_sorted, -> {order(priority: :asc).include(:routine)}
+
+  def icon
+    routine.icon
+  end
+
 end
