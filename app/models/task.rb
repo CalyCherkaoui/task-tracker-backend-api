85d05c8c2b5a8953cff7e2d9f4bc030bbfdd30ce
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-18 15:10:43 -0500

    Update task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :routine
   belongs_to :user
-  validates :name, presence: true, length: { minimum: 1 }
+  validates :name, presence: true, length: { minimum: 2 }
   validates :unit, :priority, :goal, presence: true
 end
