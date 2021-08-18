506e6bc254546f5c52848891a0b3297b92938565
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-18 18:59:17 -0500

    Migrate meusurements table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_18_200824) do
+ActiveRecord::Schema.define(version: 2021_08_18_234740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2021_08_18_200824) do
     t.string "jti", null: false
     t.datetime "exp", null: false
     t.index ["jti"], name: "index_jwt_denylist_on_jti"
+  end
+
+  create_table "mesurements", force: :cascade do |t|
+    t.integer "quantity"
+    t.bigint "task_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["task_id"], name: "index_mesurements_on_task_id"
   end
 
   create_table "routines", force: :cascade do |t|
@@ -55,6 +63,7 @@ ActiveRecord::Schema.define(version: 2021_08_18_200824) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "mesurements", "tasks"
   add_foreign_key "tasks", "routines"
   add_foreign_key "tasks", "users"
 end
