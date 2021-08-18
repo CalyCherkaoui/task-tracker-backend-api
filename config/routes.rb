ac8c2c45ce3b274c9b233c558b0bfbd26e583c0a
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-18 13:01:06 -0500

    Add routing to categories

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1, defaults: { format: :json} do
       resources :users, only: %w[show]
+      resources :categories, only: %i[index create update destroy]
     end
   end
 
