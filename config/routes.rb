9aba8fbd7038d9906c94607c8803d880d18655a5
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-18 14:19:01 -0500

    Fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1, defaults: { format: :json} do
       resources :users, only: %w[show]
-      resources :routines, only: %i[index create update destroy]
+      resources :routines, only: %i[index show create update destroy]
     end
   end
 
