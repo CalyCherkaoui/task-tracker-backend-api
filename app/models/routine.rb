e29815bdf54132214d2ed33f2021f0758daf4746
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-18 16:46:20 -0500

    Update models with relationships

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -1,6 +1,7 @@
 class Routine < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3 }
   validates :priority, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  has_many :tasks, dependent: :destroy
 
   def ordered_tasks
     tasks.ordered
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable,
          jwt_revocation_strategy: JwtDenylist
+
+  has_many :tasks
 end
