ef752b98df3e26ee559938d486605ff17048b22d
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-19 09:06:04 -0500

    Fix rubocop errors

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :task do
     # disable empty block rubocop error
     association :user, factory: :user
-    association :routine, factory: :routine,
+    association :routine, factory: :routine
   end
 end
